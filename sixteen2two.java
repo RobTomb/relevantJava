2417623a18470e4fa83081e235d76b12bc6199d2
robtomb <robtomb@outlook.com>
2017-09-20 17:00:49 +0800

    sixteen 2 two

---
+++ sixteen2two.java
@@ -0,0 +1,78 @@
+
+public class sixteen2two{
+
+	String code = "";
+	String[] encode = null;
+ 	String[] binary = {"0000" , "0001" , "0010" , "0011" , "0100" , "0101" , "0110" , "0111" , "1000" , "1001" , "1010" , "1011" , "1100" , "1101" , "1110" , "1111"};
+
+ 	public sixteen2two(){
+
+ 	}
+
+ 	public void input(String str){
+ 		code = str;
+ 	}
+
+ 	public void string2arr(){
+ 		encode = code.split("");
+ 	}
+
+ 	public void char2num(){
+
+ 		for (int i = 0 ; i <encode.length ; i++ ) {
+ 			int asc = (int)encode[i].charAt(0);
+
+ 			if( asc >= 97 && asc <= 102 )
+ 				encode[i] = (asc - 87) + "";
+
+ 			if( asc >= 65 && asc <= 70 )
+ 				encode[i] = (asc - 55) + "";
+ 		}
+ 	}
+
+ 	public void hex2binary(){
+
+ 		String[] result = new String[encode.length];
+
+ 		for ( int i = 0 ; i < encode.length ; i++ ) {
+ 			
+ 			if( !encode[i].equals(".") )
+ 				result[i] = binary[Integer.parseInt(encode[i])];
+ 			else
+ 				result[i] = encode[i];
+ 		}
+
+ 		encode = result;
+ 	}
+
+
+ 	public String show(){
+
+ 		String result = "";
+
+ 		for (String item : encode) {
+ 			result += item + " ";
+ 		}
+
+ 		return result;
+ 	}
+
+ 	public String hex2bin(){
+ 		this.string2arr();
+ 		this.char2num();
+ 		this.hex2binary();
+ 		return this.show();
+ 	}
+
+	public static void main(String args[]){
+
+		String hex1 = "a.Ab";
+		String hex2 = "B";
+		String hex3 = "7A";
+
+		sixteen2two st = new sixteen2two();
+		st.input(hex1);
+		System.out.println(st.hex2bin());
+	}
+
+}
